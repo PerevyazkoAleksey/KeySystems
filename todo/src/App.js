f0f3272e2c395874d06dddc97a34cf80503667b6
PerevyazkoAleksey <perevyazko2020@gmail.com>
2022-04-28 14:05:10 +0300

    Added a Registration and Login pages

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SignIn from './SignIn';
+import Register from './Register';
+
+function App() {
+  return (
+    <div>
+        <Register/>
+      </div>
+  );
+}
+
+export default App;
